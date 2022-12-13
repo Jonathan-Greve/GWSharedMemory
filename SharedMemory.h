af5ab165554c1f4503504443c3ebb1931770a3c9
Jonathan-Greve <jonagrev@gmail.com>
2022-12-14 06:46:21 +0800

    Minor refactor

--- SharedMemory.h
+++ SharedMemory.h
@@ -8,7 +8,7 @@ public:
     SharedMemory() = default;
     ~SharedMemory()
     {
-        //shared_memory_object::remove(m_name.c_str());
+        shared_memory_object::remove(m_name.c_str());
         if (m_is_initialized)
         {
             void_allocator void_alloc(m_connections_managed_shared_memory.get_segment_manager());
@@ -26,7 +26,8 @@ public:
         shared_memory_object::remove(m_name.c_str());
         m_managed_shared_memory = managed_shared_memory(open_or_create, m_name.c_str(), 65536);
 
-        m_connections_managed_shared_memory = managed_shared_memory(open_or_create, "connections", 65536);
+        m_connections_managed_shared_memory =
+          managed_shared_memory(open_or_create, m_connections_shared_memory_name.c_str(), 65536);
         void_allocator void_alloc(m_connections_managed_shared_memory.get_segment_manager());
 
         // Construct shared_memory singleton shared by all connected GW clients. This can be used by external processes for finding the shared memory for each client.
@@ -48,7 +49,9 @@ public:
 private:
     bool m_is_initialized{false};
 
+    std::string m_connections_shared_memory_name = "connections";
     std::string m_name;
+
     managed_shared_memory m_managed_shared_memory;
     managed_shared_memory m_connections_managed_shared_memory;
 };
